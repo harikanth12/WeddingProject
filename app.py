cec3a01631d279d5d67af86768dae3bb8951ad6a
harikanth12 <harikanth225@gmail.com>
2022-01-22 23:30:31 +0530

    image added

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask,render_template
 import os
 
 myapp = Flask(__name__)
+PEOPLE_FOLDER = os.path.join('static', 'images')
+myapp.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
 @myapp.route("/")
 def home():
-    return render_template('index.html')
+    full_filename = os.path.join(myapp.config['UPLOAD_FOLDER'], '19250.jpg')
+    scroll_image = os.path.join(myapp.config['UPLOAD_FOLDER'], '0R4A0096.jpg')
+    return render_template('index.html',user_image = full_filename,scroll_image=scroll_image)
 
 
 if __name__ == "__main__":

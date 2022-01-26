ea1051ad272268b0035497e1794f784683969c3e
harikanth12 <harikanth225@gmail.com>
2022-01-26 14:36:10 +0530

    single pics added

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ myapp.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 def home():
     full_filename = os.path.join(myapp.config['UPLOAD_FOLDER'], '19250.jpg')
     scroll_image = os.path.join(myapp.config['UPLOAD_FOLDER'], '0R4A0096.png')
-    return render_template('index.html',user_image = full_filename,scroll_image=scroll_image)
+    bottom_left_img = os.path.join(myapp.config['UPLOAD_FOLDER'], '0R4A0076.png')
+    bottom_right_img = os.path.join(myapp.config['UPLOAD_FOLDER'], '0R4A0106.png')
+    return render_template('index.html',user_image = full_filename,scroll_image=scroll_image,bottom_left_img=bottom_left_img,bottom_right_img=bottom_right_img)
 
 
 if __name__ == "__main__":

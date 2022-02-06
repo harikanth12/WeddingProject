61ac29edb3f2059186876499865f2e040ed4db99
harikanth12 <harikanth225@gmail.com>
2022-02-07 01:37:09 +0530

    changes done

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ import os
 myapp = Flask(__name__)
 PEOPLE_FOLDER = os.path.join('static', 'images')
 myapp.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
-
-
+audio_folder = os.path.join('static','audio')
+myapp.config['audio_folder'] = audio_folder
 
 @myapp.route("/")
 def home():
     full_filename = os.path.join(myapp.config['UPLOAD_FOLDER'], '19250.jpg')
-    scroll_image = os.path.join(myapp.config['UPLOAD_FOLDER'], 'banner.png')
+    scroll_image = os.path.join(myapp.config['UPLOAD_FOLDER'], 'Banner.jpeg')
     bottom_left_img = os.path.join(myapp.config['UPLOAD_FOLDER'], 'Sruthi.png')
     bottom_right_img = os.path.join(myapp.config['UPLOAD_FOLDER'], 'Hari.png')
+    audio_link = os.path.join(myapp.config['audio_folder'], 'invitation_audio.mp3')
     return render_template('index.html',user_image = full_filename,scroll_image=scroll_image,bottom_left_img=bottom_left_img,
-                           bottom_right_img=bottom_right_img)
+                           bottom_right_img=bottom_right_img,audio_link=audio_link)
+
 
 
 if __name__ == "__main__":
--- static/myscript.js
+++ static/myscript.js
@@ -21,60 +21,13 @@
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
 
-        document.getElementById('countdown').innerHTML = '<div class="countdownbox col-sm-2 col-xs-3 countdownsize"><span class="countdowninner"><font size="8" color="white">'+days+'</font><font size="2" color="white">Days</font></span></div>';
-        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3 countdownsize"><span class="countdowninner"><font size="8" color="white">'+hours+'</font><font size="2" color="white">Hrs</font></span></div>';
-        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3 countdownsize"><span class="countdowninner"><font size="8" color="white">'+minutes+'</font><font size="2" color="white">Mins</font></span></div>';
-        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3 countdownsize"><span class="countdowninner"><font size="8" color="white">'+seconds+'</font><font size="2" color="white">Secs</font></span></div>';
+        document.getElementById('countdown').innerHTML = '<div class="countdownbox col-sm-2 col-xs-3"><span><font class="text-days" color="white">'+days+'</font><font class="day-sub" color="white">Days</font></span></div>';
+        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3"><span><font class="text-days" color="white">'+hours+'</font><font class="day-sub" color="white">Hrs</font></span></div>';
+        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3"><span><font class="text-days" color="white">'+minutes+'</font><font class="day-sub" color="white">Mins</font></span></div>';
+        document.getElementById('countdown').innerHTML += '<div class="countdownbox col-sm-2 col-xs-3"><span><font class="text-days" color="white">'+seconds+'</font><font class="day-sub" color="white">Secs</font></span></div>';
     }
 
     timer = setInterval(showRemaining, 1000);
 
 
-    onload = function() {
-          if ('speechSynthesis' in window) {
-              var synth = speechSynthesis;
-    var flag = false;
 
-    /* references to the buttons */
-    var playEle = document.querySelector('#play');
-    var pauseEle = document.querySelector('#pause');
-    var stopEle = document.querySelector('#stop');
-
-    /* click event handlers for the buttons */
-    playEle.addEventListener('click', onClickPlay);
-    pauseEle.addEventListener('click', onClickPause);
-    stopEle.addEventListener('click', onClickStop);
-
-    function onClickPlay() {
-    if(!flag){
-        flag = true;
-        utterance = new SpeechSynthesisUtterance(
-              document.querySelector('article').textContent);
-        utterance.voice = synth.getVoices()[0];
-        utterance.onend = function(){
-            flag = false;
-        };
-
-        synth.speak(utterance);
-    }
-    if(synth.paused) { /* unpause/resume narration */
-        synth.resume();
-    }
-    }
-    function onClickPause() {
-    if(synth.speaking && !synth.paused){ /* pause narration */
-        synth.pause();
-    }
-    }
-    function onClickStop() {
-    if(synth.speaking){ /* stop narration */
-        /* for safari */
-        flag = false;
-        synth.cancel();
-    }
-    }
-          }
-          else {
-              /* speech synthesis not supported */
-          }
-        }
